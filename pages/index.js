fa64f3c235d3cc659e4a80749a990c43539e331b
Spence <christian.spence444@gmail.com>
2024-10-09 14:16:16 -0400

    todo-app refactoring (Form Validation)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-console.log(uuidv4());
-import {initialTodos, validationConfig} from "../utils/constants.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
+import FormValidator from "../components/FormValidator.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -17,29 +18,10 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-// The logic in this function should all be handled in the Todo class.
- const generateTodo = (data) => {
-  const todo = new Todo (data,"#todo-template");
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
   return todoElement;
-//   todoNameEl.textContent = data.name;
-//   todoCheckboxEl.checked = data.completed;
-
-//   // Apply id and for attributes.
-//   // The id will initially be undefined for new todos.
-
-
-//   // If a due date has been set, parsing this it with `new Date` will return a
-//   // number. If so, we display a string version of the due date in the todo.
-//   const dueDate = new Date(data.date);
-//   if (!isNaN(dueDate)) {
-//     todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
-//       year: "numeric",
-//       month: "short",
-//       day: "numeric",
-//     })}`;
-//   }
-
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -55,7 +37,6 @@ addTodoForm.addEventListener("submit", (evt) => {
   const name = evt.target.name.value;
   const dateInput = evt.target.date.value;
 
-  // Create a date object and adjust for timezone
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
@@ -64,9 +45,14 @@ addTodoForm.addEventListener("submit", (evt) => {
   const todo = generateTodo(values);
   todosList.append(todo);
   closeModal(addTodoPopup);
+
+  todoValidator.resetValidation();
 });
 
 initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
+const todoValidator = new FormValidator(validationConfig, addTodoForm);
+todoValidator.enableValidation();
